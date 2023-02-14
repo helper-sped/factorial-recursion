19c0f352288f08a89aab4247f87cd0571c714fd5
sped <106504754+helper-sped@users.noreply.github.com>
2023-02-14 15:11:10 -0800

    Update Recursion.java

--- Recursion.java
+++ Recursion.java
@@ -1,4 +1,1 @@
-public int recursionImprovedAgain(int i) {
-  if (i == 1) return 1;                     
-  return p * recursionImprovedAgain(i-1);
-}
+public int recursionImprovedAgain(int i) {return i=(i==1)?1:i*recursionIprovedAgain(i-1);}
